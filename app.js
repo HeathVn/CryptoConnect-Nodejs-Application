db0d623272f8d17c0fd744f4420c9e926c4bc90c
HeathVn <62621793+HeathVn@users.noreply.github.com>
2022-03-23 17:01:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.set('view engine', 'ejs');
 
 
 //connect to database
-mongoose.connect('mongodb://localhost:27017/connect',
+mongoose.connect('mongodb://localhost:27017/connect?directConnection=true',
                 {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 .then(()=>{
     //start the server
